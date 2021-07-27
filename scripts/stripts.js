ada4bf83cacf5720f769bd18e08fcd3503098127
Yasmim <yasmimcavalcanti.r@gmail.com>
2021-07-27 15:33:13 -0300

    Added script to display cards according to user input

--- scripts/stripts.js
+++ scripts/stripts.js
@@ -3,9 +3,11 @@ initGame();
 function initGame() {
     let cardsNumber;
     do {
-        cardsNumber = prompt("Com quantas cartas você quer jogar? Escolha um número par.");
-        if (((cardsNumber % 2) === 0) && cardsNumber === "0") {
+        cardsNumber = Number(prompt("Com quantas cartas você quer jogar? Escolha um número par de 4 a 14."));
+        console.log(cardsNumber);
+        if (((cardsNumber % 2) !== 0) || cardsNumber < 4 || cardsNumber > 14) {
             cardsNumber = null;
+            console.log("teste")
         }     
     } while (!cardsNumber);
     startGame(cardsNumber);
@@ -13,4 +15,16 @@ function initGame() {
 
 function startGame(cardsNumber) {
     alert("O jogo vai começar com " + cardsNumber + " cartas!");
+
+    addCards(cardsNumber);
+}
+
+function addCards(number) {
+    const gameContent = document.querySelector(".game-content");
+    for (let i = 0; i < number; i++) {
+        gameContent.innerHTML = gameContent. innerHTML + 
+        '<div class="card">' +  
+            '<img src="assets/front.png" alt="" srcset="">' +
+        '</div>'        
+    }
 }
